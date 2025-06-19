d42a7c92d4d7d9cb4f0c9507741dc9e6a6a4827f
motinxyz <motinsghs@gmail.com>
2025-06-19 14:57:27 +0600

    hello modified

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello, jenkins!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It prints a greeting message to the console.");
     }
 }
